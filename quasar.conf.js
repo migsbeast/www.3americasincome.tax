2be44fae1dc60b4b87f03c6b5019c24bd863962b
migsbeast <miguelrod.dev@gmail.com>
2021-02-10 21:00:28 -0600

    Added appropiate resources

--- quasar.conf.js
+++ quasar.conf.js
@@ -19,7 +19,7 @@ module.exports = function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/boot-files
     boot: [
-
+      'awsAmplify',
       'axios'
     ],
 
@@ -99,7 +99,7 @@ module.exports = function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ['Dialog', 'Loading']
     },
 
     // animations: 'all', // --- includes all animations
@@ -116,9 +116,9 @@ module.exports = function (/* ctx */) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: 'Quasar App',
-        short_name: 'Quasar App',
-        description: 'A Quasar Framework app',
+        name: '3 Americas Income Tax & Multiservices ',
+        short_name: '3 Americas Income Tax',
+        description: 'A web app for the company to see clients docs',
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',

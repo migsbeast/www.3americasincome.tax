819007bec5725dee9d4d98ca08c11a38fd114e26
migsbeast <miguelrod.dev@gmail.com>
2021-02-10 21:02:25 -0600

    Simplify mainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,35 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-        <!-- mobile menu <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        /> -->
-
-    <!-- mobile menu<q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer> -->
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -37,82 +7,12 @@
 </template>
 
 <script>
-import { scroll } from 'quasar'
-const { getScrollTarget, setScrollPosition } = scroll
-const englishTabs = [
-  {
-    title: 'Home',
-    link: 'https://www.3americasincometax.com'
-  },
-  {
-    title: 'Services'
-  },
-  {
-    title: 'About Us'
-  },
-  {
-    title: 'Team'
-  },
-  {
-    title: 'Reviews'
-  }
-]
-const spanishTabs = [
-  {
-    title: 'Inicio',
-    link: 'https://www.3americasincometax.com'
-  },
-  {
-    title: 'Servicios'
-  },
-  {
-    title: 'Sobre Nosotros'
-  },
-  {
-    title: 'El Equipo'
-  },
-  {
-    title: 'Recomendaciónes'
-  }
-]
-
 export default {
   name: 'MainLayout',
   data () {
     return {
-      leftDrawerOpen: false,
-      selectedLanguage: 'English',
-      languages: ['Español', 'English'],
-      esLabel: 'Español',
-      enLabel: 'English'
-    }
-  },
-  methods: {
-    tabClicked (id, referenceElement) {
-      const el = document.getElementById(referenceElement)
-      const target = getScrollTarget(el)
-      const offset = el.offsetTop + 3
-      const duration = 900
-      setScrollPosition(target, offset, duration)
-    },
-    switchLanguage () {
-      if (this.selectedLanguage === 'English') {
-        this.selectedLanguage = 'Español'
-      } else if (this.selectedLanguage === 'Español') {
-        this.selectedLanguage = 'English'
-      }
-    }
-  },
-  computed: {
-    languageLabel () {
-      return this.selectedLanguage === 'English' ? this.enLabel : this.esLabel
-    },
-    menuTabs () {
-      return this.selectedLanguage === 'English' ? englishTabs : spanishTabs
+
     }
   }
 }
 </script>
-
-<style>
-</style>

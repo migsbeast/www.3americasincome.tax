b60adea80b0ddb4b287176718121719dd68f2f57
migsbeast <miguelrod.dev@gmail.com>
2021-03-04 15:18:50 -0600

    Removed contact us slide

--- src/store/slider/state.js
+++ src/store/slider/state.js
@@ -10,13 +10,6 @@ export default function () {
           },
           {
             id: 2,
-            subtitle1: 'Let us help with your Income Tax Return!',
-            subtitle2: 'Free Estimates',
-            buttonLabel: 'Fill Out This Form',
-            buttonLink: ''
-          },
-          {
-            id: 3,
             subtitle1: 'Check-In - Like Our FB Page  - Share',
             subtitle2: 'Like Our Page For a $10 discount and leave us a Google Review for a special offer!',
             buttonLabel: 'Like Our Page',
@@ -37,13 +30,6 @@ export default function () {
           },
           {
             id: 2,
-            subtitle1: '¡Déjanos ayudarte con tus Impuestos!',
-            description: '(Mandanos tus documentos cuando haces un acuenta en nostro sitio)',
-            buttonLabel: 'Llene Esta Forma',
-            buttonLink: ''
-          },
-          {
-            id: 3,
             subtitle1: 'Dele Like a nuestra pagina de Facebook, ',
             subtitle2: 'y escriba una recomendación en Google ',
             subtitle3: 'para un descuento de $10!',
